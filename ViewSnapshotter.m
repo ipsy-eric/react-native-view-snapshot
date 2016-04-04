6e271d56cbdcc33624bd74ea54f1c6992416e98e
Eric Goldberg <eric.goldberg@gmail.com>
2016-04-04 16:00:37 -0700

    Changed for our specific use case: looks for UIWebView's scrollView and snapshots it

--- ViewSnapshotter.m
+++ ViewSnapshotter.m
@@ -2,6 +2,7 @@
 #import "RCTConvert.h"
 #import "RCTBridge.h"
 #import "RCTUIManager.h"
+#import "RCTWebView.h"
 
 @implementation ViewSnapshotter
 
@@ -11,35 +12,69 @@ RCT_EXPORT_MODULE()
 
 - (dispatch_queue_t)methodQueue
 {
-  return dispatch_get_main_queue();
+    return dispatch_get_main_queue();
 }
 
 RCT_EXPORT_METHOD(saveSnapshotToPath:(nonnull NSNumber *)reactTag
                   path:(NSString *)filePath
                   callback:(RCTResponseSenderBlock)callback)
 {
+    
+    UIView *view = [self.bridge.uiManager viewForReactTag:reactTag];
+    
+    UIScrollView* scrollView = [self findScrollViewForWebViewInViewHierarchy:view];
+    
+    if (nil == scrollView) {
+        NSLog(@"Error: could not find scrollview to snapshot!");
+        return callback(@[@"Error: could not find scrollview to snapshot!"]);
+    }
+    
+    UIImage* image = nil;
+    UIGraphicsBeginImageContext(scrollView.contentSize);
+    {
+        CGPoint savedContentOffset = scrollView.contentOffset;
+        CGRect savedFrame = scrollView.frame;
+        
+        scrollView.contentOffset = CGPointZero;
+        scrollView.frame = CGRectMake(0, 0, scrollView.contentSize.width, scrollView.contentSize.height);
+        
+        [scrollView.layer renderInContext:UIGraphicsGetCurrentContext()];
+        image = UIGraphicsGetImageFromCurrentImageContext();
+        
+        scrollView.contentOffset = savedContentOffset;
+        scrollView.frame = savedFrame;
+    }
+    UIGraphicsEndImageContext();
+    
+    NSData *data = UIImagePNGRepresentation(image);
+    
+    NSError *error;
+    
+    BOOL writeSucceeded = [data writeToFile:filePath options:0 error:&error];
+    
+    if (!writeSucceeded) {
+        return callback(@[[NSString stringWithFormat:@"Could not write file at path %@", filePath]]);
+    }
+    
+    callback(@[[NSNull null], [NSNumber numberWithBool:writeSucceeded]]);
+}
 
-  UIView *view = [self.bridge.uiManager viewForReactTag:reactTag];
-
-  // defaults: snapshot the same size as the view, with alpha transparency, with current device's scale factor
-  UIGraphicsBeginImageContextWithOptions(view.frame.size, NO, 0.0);
-
-  [view drawViewHierarchyInRect:CGRectMake(0, 0, view.frame.size.width, view.frame.size.height) afterScreenUpdates:YES];
-  UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-
-  UIGraphicsEndImageContext();
-
-  NSData *data = UIImagePNGRepresentation(image);
-
-  NSError *error;
-
-  BOOL writeSucceeded = [data writeToFile:filePath options:0 error:&error];
-
-  if (!writeSucceeded) {
-    return callback(@[[NSString stringWithFormat:@"Could not write file at path %@", filePath]]);
-  }
-
-  callback(@[[NSNull null], [NSNumber numberWithBool:writeSucceeded]]);
+- (UIScrollView *)findScrollViewForWebViewInViewHierarchy:(UIView *)view
+{
+    UIScrollView* targetScrollView = nil;
+    
+    for (UIView* subview in view.subviews) {
+        if ([subview isKindOfClass:[RCTWebView class]]) {
+            for (UIView* childView in subview.subviews) {
+                if ([childView isKindOfClass:[UIWebView class]]) {
+                    NSLog(@"Found the target webview: %@", childView);
+                    targetScrollView = [(UIWebView *)childView scrollView];
+                }
+            }
+        }
+    }
+    
+    return targetScrollView;
 }
 
 @end
